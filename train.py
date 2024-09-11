e33f30847b28cd4fcd7c963af7ac3ba7fe87804c
3ming <873118475@qq.com>
2024-09-11 10:59:36 +0800

    add

--- train.py
+++ train.py
@@ -3,17 +3,17 @@ warnings.filterwarnings('ignore')
 from ultralytics import RTDETR
 
 if __name__ == '__main__':
-    # 每次记得把配置文件的nc改为15
-    model = RTDETR('ultralytics/cfg/models/rt-detr/rtdetr-DRBNCSPELAN-ContextGuidedDown.yaml')
+    model = RTDETR('ultralytics\cfg\models\\rt-detr\\rtdetr-r18.yaml')
     # model.load('weights/rtdetr-r18.pt')  # loading pretrain weights
     model.train(data='dataset/data.yaml',
                 cache=False,
                 imgsz=640,
-                epochs=500,
-                batch=16,
+                epochs=5,
+                batch=8,
                 workers=8,
                 device='0',
                 # resume='', # last.pt path  # 断点续训
-                project='runs/train',
-                name='DRBNCSPELAN-ContextGuidedDown',
+                project='runs/train_local',
+                name='r-18',
                 )
+
