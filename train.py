bae52d4efd05606ef1a2dde04d6788b21f51e4bf
3ming <873118475@qq.com>
2024-09-11 11:25:28 +0800

    1

--- train.py
+++ train.py
@@ -3,17 +3,17 @@ warnings.filterwarnings('ignore')
 from ultralytics import RTDETR
 
 if __name__ == '__main__':
-    model = RTDETR('ultralytics\cfg\models\\rt-detr\\rtdetr-r18.yaml')
+    model = RTDETR('ultralytics\cfg\models\\rt-detr\\rtdetr-Faster-goldyolo-DRBC3')
     # model.load('weights/rtdetr-r18.pt')  # loading pretrain weights
     model.train(data='dataset/data.yaml',
                 cache=False,
                 imgsz=640,
-                epochs=5,
+                epochs=250,
                 batch=8,
                 workers=8,
                 device='0',
                 # resume='', # last.pt path  # 断点续训
                 project='runs/train_local',
-                name='r-18',
+                name='Faster-goldyolo-DRBC3',
                 )
 
